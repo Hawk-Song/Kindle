25f1425b67a59b9b2221e92c5df8d33a04258b68
Hawk Song <savokisong@gmail.com>
2018-08-03 16:10:55 -0700

    custom cell layout

--- Kindle/BookCell.swift
+++ Kindle/BookCell.swift
@@ -10,8 +10,57 @@ import UIKit
 
 class BookCell: UITableViewCell {
     
+    var book: Book? {
+        didSet {
+            coverImageView.image = book?.image
+            titleLabel.text = book?.title
+            authorLabel.text = book?.author
+        }
+    }
+    
+    private let coverImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        imageView.image = #imageLiteral(resourceName: "steve_jobs")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "This is the text for the title of our book inside of our cell"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let authorLabel: UILabel = {
+        let label = UILabel()
+        label.text = "This is some author for the book that we have in this row"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(coverImageView)
+        coverImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
+        coverImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 8).isActive = true
+        coverImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8).isActive = true // if the anchor is bottom, the constant must be negative, because it is the opposite direction
+        coverImageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        addSubview(titleLabel)
+        titleLabel.leftAnchor.constraint(equalTo: coverImageView.rightAnchor, constant: 8).isActive = true
+        titleLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8).isActive = true // push it to left 8 pixcels, so if use rightAnchor, use negative
+        titleLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -10).isActive = true
+        
+        addSubview(authorLabel)
+        authorLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4).isActive = true
+        authorLabel.leftAnchor.constraint(equalTo: coverImageView.rightAnchor, constant: 8).isActive = true
+        authorLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8).isActive = true
+        authorLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
